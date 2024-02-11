4b42cf48033cf06ae3c4f79d11c6d2b4ab5f2298
valeeriiiee <obleavalerie@gmail.com>
2024-02-11 14:25:10 +0800

    Update task-07.js

---
+++ src/js/task-07.js
@@ -0,0 +1,6 @@
+const fontSizeControl = document.getElementById('font-size-control');
+const textElement = document.getElementById('text');
+
+fontSizeControl.addEventListener('input', function() {
+  textElement.style.fontSize = fontSizeControl.value + 'px';
+});
