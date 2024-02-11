09b4b029e32205432aac69b37a088321695a73b5
valeeriiiee <obleavalerie@gmail.com>
2024-02-11 14:23:35 +0800

    updated common css  file and task 6 js file

---
+++ src/js/task-06.js
@@ -0,0 +1,14 @@
+const validationInput = document.getElementById('validation-input');
+
+validationInput.addEventListener('blur', function() {
+  const inputValue = validationInput.value.trim();  
+  const desiredLength = parseInt(validationInput.getAttribute('data-length'));
+
+  if (inputValue.length === desiredLength) {
+    validationInput.classList.add('valid');
+    validationInput.classList.remove('invalid');
+  } else {
+    validationInput.classList.add('invalid');
+    validationInput.classList.remove('valid');
+  }
+});
