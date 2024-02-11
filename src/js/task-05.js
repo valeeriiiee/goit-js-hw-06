87560335c284f3c1ce6bfd1f3d26f6c65e233d2d
valeeriiiee <obleavalerie@gmail.com>
2024-02-11 14:14:43 +0800

    Update task-05.js

---
+++ src/js/task-05.js
@@ -0,0 +1,10 @@
+const nameInput = document.getElementById('name-input');
+const nameOutput = document.getElementById('name-output');
+
+nameInput.addEventListener('input', function() {
+  if (nameInput.value.trim() === '') {
+    nameOutput.textContent = 'Anonymous';
+  } else {
+    nameOutput.textContent = nameInput.value;
+  }
+});
