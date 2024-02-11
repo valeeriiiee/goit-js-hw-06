3161540925db3c2b2e9f68a006ec6d253a9f2ded
valeeriiiee <obleavalerie@gmail.com>
2024-02-11 14:29:56 +0800

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const colorSpan = document.querySelector('.color');
+  const changeColorButton = document.querySelector('.change-color');
+
+  changeColorButton.addEventListener('click', function () {
+    const randomColor = getRandomHexColor();
+    document.body.style.backgroundColor = randomColor;
+    colorSpan.textContent = randomColor;
+  });
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
